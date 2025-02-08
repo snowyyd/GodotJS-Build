3cea11889ee392b524c24a8360703ca734462bed
ialex32x <lin_032@hotmail.com>
2025-02-08 18:43:26 +0800

    use vulkansdk 1.4.304.0

--- install_vulkan_sdk_macos.sh
+++ install_vulkan_sdk_macos.sh
@@ -3,14 +3,20 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-# Download and install the Vulkan SDK.
-curl -L "https://sdk.lunarg.com/sdk/download/latest/mac/vulkan-sdk.zip" -o /tmp/vulkan-sdk.zip
+# # Download and install the Vulkan SDK. (1.4.304.1)
+# curl -L "https://sdk.lunarg.com/sdk/download/1.4.304.1/mac/vulkansdk-macos-1.4.304.1.zip" -o /tmp/vulkan-sdk.zip
+# unzip /tmp/vulkan-sdk.zip -d /tmp
+# /tmp/InstallVulkan-1.4.304.1.app/Contents/MacOS/InstallVulkan-1.4.304.1 \
+#     --accept-licenses --default-answer --confirm-command install
+# rm -rf /tmp/InstallVulkan-1.4.304.1.app
+
+# Download and install the Vulkan SDK. (1.4.304.0)
+curl -L "https://vulkan.lunarg.com/sdk/download/1.4.304.0/mac/vulkansdk-macos-1.4.304.0.zip" -o /tmp/vulkan-sdk.zip
 unzip /tmp/vulkan-sdk.zip -d /tmp
-/tmp/InstallVulkan.app/Contents/MacOS/InstallVulkan* \
+/tmp/InstallVulkan.app/Contents/MacOS/InstallVulkan \
     --accept-licenses --default-answer --confirm-command install
-
-
 rm -rf /tmp/InstallVulkan.app
-rm -f /tmp/vulkan-sdk.zip
 
+
+rm -f /tmp/vulkan-sdk.zip
 echo 'Vulkan SDK installed successfully! You can now build Godot by running "scons".'
